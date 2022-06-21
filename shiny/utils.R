2584a0a8f8e1d85292f56c36be248c05867c940a
const-ae <artjom31415@googlemail.com>
2022-06-21 14:34:02 +0200

    Move shiny app into a project by itself

--- shiny/utils.R
+++
@@ -1,5 +0,0 @@
-div_with_floating_gear <- function(panel, menu_content){
-  div(class = "floating_gear_holder",
-      div(class = "floating_gear", shinyWidgets::dropdownButton(menu_content, icon = icon("gear"), size = "sm", right = TRUE)),
-      panel)
-}
