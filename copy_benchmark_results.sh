410dfe8a8ab81d339b3073cfbcc0f4792735d79d
const-ae <artjom31415@googlemail.com>
2022-07-19 17:56:54 +0200

    Add renv/activate.R into repository

--- copy_benchmark_results.sh
+++ copy_benchmark_results.sh
@@ -4,8 +4,10 @@
 rm -r benchmark
 # Make new folder
 mkdir benchmark
+mkdir benchmark/renv
 
 # Copy relevant files (i.e., exclude renv library because it is too big)
 scp -r ahlmanne@seneca:~/projects/transformGamPoi-Paper2/{README.md,job_overview.yaml,renv.lock,.Rprofile} benchmark/.
 scp -r ahlmanne@seneca:~/projects/transformGamPoi-Paper2/src benchmark/src
+scp -r ahlmanne@seneca:~/projects/transformGamPoi-Paper2/renv/activate.R benchmark/renv/activate.R
 scp -r ahlmanne@seneca:~/projects/transformGamPoi-Paper2/output benchmark/output
